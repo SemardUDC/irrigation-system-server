8d48781a2f14f5db9bc2abbf3561976af0498899
Humberto Leal <humberto.adolfo@hotmail.com>
2018-11-05 15:07:22 -0500

    Actualizacion de controlador para uso de creador de query desde utils

--- server/controllers/sensor.controller.js
+++ server/controllers/sensor.controller.js
@@ -1,48 +1,15 @@
 const WaterFlow = require('../models/sensors/water-flow.model');
 const Ultrasonic = require('../models/sensors/ultrasonic.model');
-const moment = require('moment');
-
-function createQueryCondition(time, value, identification = undefined) {
-    let dateSelected = undefined;
-    let queryCondition = (identification) ? { identification } : {};
-
-    if (time === 'today') {
-        dateSelected = moment(Date.now());
-    } else if (time === 'day') {
-        if (!value) {
-            throw new Error(`Uncompleted query parameters, received time but no value.`);
-        }
-        dateSelected = moment(value, 'YYYYMMDD');
-        if (!dateSelected.isValid()) {
-            throw new Error(`Invalid query 'value' when parsing to a date: ${value}`);
-        }
-    }
-
-    if (dateSelected) {
-        queryCondition.date = {
-            $gte: dateSelected.startOf('day').valueOf(), $lte: dateSelected.endOf('day').valueOf()
-        }
-    }
-
-    return queryCondition;
-}
+const {createQueryCondition} = require('../utils/models');
 
 async function getWaterFlowRecords(time, value, count, identification = undefined) {
     const queryCondition = createQueryCondition(time, value, identification);
-    const waterRecords = await WaterFlow.find(queryCondition).limit(count).sort({ date: 'desc' });
-    if (!waterRecords) {
-        throw new Error('No water-flow record was found.');
-    }
-    return waterRecords;
+    return await WaterFlow.find(queryCondition).limit(count).sort({ date: 'desc' });
 }
 
 async function getUltrasonicRecords(time, value, count, identification = undefined) {
     const queryCondition = createQueryCondition(time, value, identification);
-    const ultrasonicRecords = await Ultrasonic.find(queryCondition).limit(count).sort({ date: 'desc' });
-    if (!ultrasonicRecords) {
-        throw new Error('No ultrasonic record was found.');
-    }
-    return ultrasonicRecords;
+    return await Ultrasonic.find(queryCondition).limit(count).sort({ date: 'desc' });
 }
 
 module.exports = {
