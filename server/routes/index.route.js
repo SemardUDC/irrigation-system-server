c2f5543e24da10c4c4dc47b3ee6d8738a9f842b1
Humberto Leal <humberto.adolfo@hotmail.com>
2018-11-05 21:00:22 -0500

    Eliminacion rutas y controladores de plantilla

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const userRoutes = require('./user.route');
-const authRoutes = require('./auth.route');
 const sensorRoutes = require('./sensor/index.route');
 const actuatorRoutes = require('./actuator/index.route');
 
@@ -11,8 +9,6 @@ router.get('/health-check', (req, res) =>
   res.send('OK')
 );
 
-router.use('/auth', authRoutes);
-router.use('/user', userRoutes);
 router.use('/sensor', sensorRoutes);
 router.use('/actuator', actuatorRoutes);
 
