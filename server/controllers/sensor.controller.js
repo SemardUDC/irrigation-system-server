c2674acacecbe025a7669a79946449e79dbd3c9c
Humberto Leal <humberto.adolfo@hotmail.com>
2018-11-05 21:11:25 -0500

    Correccion de controlador para ultrasonic

--- server/controllers/sensor.controller.js
+++ server/controllers/sensor.controller.js
@@ -1,15 +1,21 @@
 const WaterFlow = require('../models/sensors/water-flow.model');
 const Ultrasonic = require('../models/sensors/ultrasonic.model');
-const {createQueryCondition} = require('../utils/models');
+const { createQueryCondition } = require('../utils/models');
 
 async function getWaterFlowRecords(time, value, count, identification = undefined) {
     const queryCondition = createQueryCondition(time, value, identification);
     return await WaterFlow.find(queryCondition).limit(count).sort({ date: 'desc' });
 }
 
-async function getUltrasonicRecords(time, value, count, identification = undefined) {
+async function getUltrasonicRecords(req, res) {
+    const time = String(req.query.time);
+    const value = Number(req.query.value);
+    const count = Number(req.query.count);
+    const identification = req.params.identification;
+
     const queryCondition = createQueryCondition(time, value, identification);
-    return await Ultrasonic.find(queryCondition).limit(count).sort({ date: 'desc' });
+    const ultrasonicRecords = await Ultrasonic.find(queryCondition).limit(count).sort({ date: 'desc' });
+    res.send({ ultrasonicRecords });
 }
 
 module.exports = {
--- server/routes/sensor/ultrasonic.route.js
+++ server/routes/sensor/ultrasonic.route.js
@@ -4,23 +4,7 @@ const sensorCtrl = require('../../controllers/sensor.controller');
 
 const router = express.Router();
 
-router.get('/', asyncHandler(async (req, res) => {
-    const time = String(req.query.time);
-    const value = Number(req.query.value);
-    const count = Number(req.query.count);
-
-    const ultrasonicRecords = await sensorCtrl.getUltrasonicRecords(time, value, count);
-    res.send({ultrasonicRecords});
-}));
-
-router.get('/:identification', asyncHandler(async (req, res) => {
-    const time = String(req.query.time);
-    const value = Number(req.query.value);
-    const count = Number(req.query.count);
-    const identification = req.params.identification;
-
-    const ultrasonicRecords = await sensorCtrl.getUltrasonicRecords(time, value, count, identification);
-    res.send({ultrasonicRecords});
-}));
+router.get('/', asyncHandler(sensorCtrl.getUltrasonicRecords));
+router.get('/:identification', asyncHandler(sensorCtrl.getUltrasonicRecords));
 
 module.exports = router;
