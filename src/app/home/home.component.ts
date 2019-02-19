97e273c401dfa8491d3612adfd7f1e25dd7ccd27
Daniel Pombo <pombodaniel10@gmail.com>
2019-02-19 15:45:36 -0500

    pruebas con los charts de Google y de Fusion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,6 +14,34 @@ export class HomeComponent implements OnDestroy {
 
   private subscription: Subscription;
   public message: string;
+  phType: string = "Gauge"
+  phData:Array<Array<any>> = [['Nivel de PH',7]];
+  phOptions: {} = {
+    width: 400, 
+    height: 150,
+    max: 14
+  }
+
+  tempdata: Object = {
+    "chart": {
+        "lowerLimit": "50",
+        "upperLimit": "0",
+        "decimals": "5",
+        "numberSuffix": "°C",
+        "showhovereffect": "1",
+        "thmFillColor": "#008ee4",
+        "showGaugeBorder": "1",
+        "gaugeBorderColor": "#008ee4",
+        "gaugeBorderThickness": "2",
+        "gaugeBorderAlpha": "30",
+        "thmOriginX": "100",
+        "chartBottomMargin": "20",
+        "valueFontColor": "#000000",
+        "showValue": "1"
+    },
+    "value": "32",
+
+}
 
   constructor(private _mqttService: MqttService) {
     this.subscription = this._mqttService.observe('my/topic').subscribe((message: IMqttMessage) => {
