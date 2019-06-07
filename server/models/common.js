9702f1e0c6cc1e0238bf3e6a0d5d931c3b563dad
Humberto Leal <humberto.adolfo@hotmail.com>
2019-06-06 20:22:12 -0500

    Correccion de bug en función para encontrar últimos registros para cada id de cada sensor/actuador

--- server/models/common.js
+++ server/models/common.js
@@ -1,13 +1,21 @@
+'use-strict'
+
 module.exports = {
     findLastRecordForEachId: async function() {
         const ids = await this.distinct('identification');
         const limit = 1;
-        console.log('Identifications found', ids);
-    
+        console.log(`Identifications found ${ids} for ${this.collection.name}`);
+        if (typeof ids[0] === 'number')
+            console.log('First found is number');
+        
         let lastRecords = {};
         let record = null;
-        for(id of ids) {
+        // Without the 'const' or 'let' in 'id', the results were mismatched
+        // with the ids being different.
+        for(const id of ids) {
+            
             record = await this.find({identification: id}).sort({date: 'desc'}).limit(limit);
+            console.log(`Record found for: ${id} in ${this.collection.name} details: ${record}`);
             lastRecords[id] = record;
         }
     
