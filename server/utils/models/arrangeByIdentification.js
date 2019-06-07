b49d4408d9e269153a31675e742d3b98d518c190
Humberto Leal <humberto.adolfo@hotmail.com>
2019-06-06 20:58:26 -0500

    Documentacion de función

--- server/utils/models/arrangeByIdentification.js
+++ server/utils/models/arrangeByIdentification.js
@@ -1,8 +1,8 @@
 
 /**
- * Receives an array of records an arrange them exatracting the
- * identification field and put them under a field corresponding
- * to the same identification extracted.
+ * Receives an array of records and it arranges them by extracting
+ * the identification field and putting the remaining attributes under
+ * a field corresponding to the same identification extracted.
  * @param {Array of records containing a field called identification} records 
  */
 function arrangeByIdentification(records) {
