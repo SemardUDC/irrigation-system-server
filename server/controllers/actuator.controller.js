2aebe2fd4ea68b9a1587375a3cc7fb957cb3f17b
Humberto Leal <humberto.adolfo@hotmail.com>
2019-06-06 21:29:44 -0500

    Controlador pump-motor

--- server/controllers/actuator.controller.js
+++ server/controllers/actuator.controller.js
@@ -1,4 +1,5 @@
 const SolenoidValve = require('../models/actuators/solenoid-valve.model');
+const PumpMotor = require('../models/actuators/pump-motor.model');
 const mqttClient = require('../config/mqtt');
 const { createQueryCondition } = require('../utils/models');
 
@@ -39,7 +40,45 @@ async function manipulateSolenoidValve(req, res) {
     }
 }
 
+async function getPumpMotorRecords(req, res) {
+    const time = String(req.query.time);
+    const value = String(req.query.value);
+    const count = String(req.query.count);
+    const identification = req.params.identification;
+
+    const queryCondition = createQueryCondition(time, value, identification);
+    const pumpMotorRecords = await PumpMotor.find(queryCondition).limit(count).sort({ date: 'desc' });
+
+    res.send({ pumpMotorRecords });
+}
+
+async function manipulatePumpMotor(req, res) {
+    const identification = (req.params.identification) ? Number(req.params.identification) : '*';
+    const action = req.body.action;
+    console.log('Identification is: ', identification);
+    console.log('Action is: ', action);
+    if (!['activate', 'deactivate'].includes(action)) {
+        return res.status(400).send(`Action passed is invalid. Must be 'activate' or 'deactivate', was: ${action}`);
+    }
+
+    const message = {
+        identification,
+        action
+    }
+
+    const messageJSON = JSON.stringify(message);
+
+    try {
+        await mqttClient.publish('irrigation-system/actuator/pump-motor/set', messageJSON);
+        return res.send({ pumpMotor: { ...message, response: 'Message sent successfully to mqtt topic' } });
+    } catch (e) {
+        res.status(400).send({ message: `An error ocurred while publishing the message ${err}` });
+    }
+}
+
 module.exports = {
     getSolenoidValveRecords,
-    manipulateSolenoidValve
+    manipulateSolenoidValve,
+    getPumpMotorRecords,
+    manipulatePumpMotor
 }
